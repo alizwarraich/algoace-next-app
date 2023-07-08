0d0db8c322782fa37bb64baac6b5ee1f59ab9017
Ali Zulqarnain <warraichleopard@gmail.com>
2023-07-08 16:41:03 +0500

    Done with Hero section

--- app/page.js
+++ app/page.js
@@ -1,16 +1,26 @@
-import Link from "next/link";
+import Image from "next/image";
 
 const Homepage = () => {
     return (
         <div>
-            <h1>Welcome to Next.js App</h1>
-            <ul>
-                <li>
-                    <Link className="text-blue-600 underline" href="/about">
-                        About &rarr;
-                    </Link>
-                </li>
-            </ul>
+            {/* Hero Section */}
+            <div className="grid grid-cols-[50%_50%]">
+                <div className="bg-[#004D6D] text-white px-40 py-20 flex flex-col gap-8">
+                    <p className="font-medium text-3xl capitalize">
+                        Lorem ipsum dolor sit amet consectetur.
+                    </p>
+                    <p className="text-2xl capitalize">
+                        Lorem ipsum dolor sit amet consectetur. Viverr
+                        scelerisqu.
+                    </p>
+                </div>
+                <div className="w-full h-full relative">
+                    <Image src="/hero.png" alt="Hero Image" layout="fill" />
+                </div>
+            </div>
+
+            {/* Featured Books */}
+            <div></div>
         </div>
     );
 };
